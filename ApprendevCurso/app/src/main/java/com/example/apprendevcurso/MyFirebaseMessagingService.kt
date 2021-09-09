d85aedef339750b1f8f4f99c1752f603fb85f38e
andy <alemanmartineza@gmail.com>
2021-09-08 19:33:25 -0500

    FirebaseLoging

---
+++ ApprendevCurso/app/src/main/java/com/example/apprendevcurso/MyFirebaseMessagingService.kt
@@ -0,0 +1,18 @@
+package com.example.apprendevcurso
+
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Looper.prepare()
+
+        Handler().post {
+            Toast.makeText(baseContext, remoteMessage.notification?.title, Toast.LENGTH_SHORT).show()
+        }
+        Looper.loop()
+    }
+}
--- ApprendevCurso/app/src/main/java/com/example/apprendevcurso/ProfileActivity.kt
+++ ApprendevCurso/app/src/main/java/com/example/apprendevcurso/ProfileActivity.kt
@@ -2,40 +2,40 @@ package com.example.apprendevcurso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.apprendevcurso.profile.Profile
+import com.example.apprendevcurso.databinding.ActivityProfileBinding
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType {
+    BASIC,
+    FACEBOOK
+}
 
 class ProfileActivity : AppCompatActivity() {
+    private var _binding: ActivityProfileBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-        val profile = intent.getParcelableExtra<Profile>("Profile")
-        Log.d(
-            ProfileActivity::class.java.simpleName,
-            "profileId = ${profile?.profileId}"
-        )
-        Log.d(
-            ProfileActivity::class.java.simpleName,
-            "userName = ${profile?.userName}"
-        )
-        Log.d(
-            ProfileActivity::class.java.simpleName,
-            "name = ${profile?.name}"
-        )
-        Log.d(
-            ProfileActivity::class.java.simpleName,
-            "lastName = ${profile?.lastName}"
-        )
-        Log.d(
-            ProfileActivity::class.java.simpleName,
-            "userDescription = ${profile?.userDescription}"
-        )
+        _binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        intent.extras.let {
+            setUp(
+                it?.getString("email") ?: "",
+                it?.getString("provider") ?: ""
+            )
+        }
+    }
 
-        /**
-         * Van asignar cada valor del Profile en un TextView deben ser un total
-         * de 5 TextViews
-         * Id Perfil: 1
-         * Nombre de usuario: Andy
-         */
+    private fun setUp(email: String, provider: String) {
+        title = "Perfil de usuario"
+        binding.emailText.text = email
+        binding.providerText.text = provider
+        binding.signoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            if (provider == ProviderType.FACEBOOK.name) {
+                LoginManager.getInstance().logOut()
+            }
+            onBackPressed()
+        }
     }
 }
